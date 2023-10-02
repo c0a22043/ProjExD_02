a175a7ae40b50ca7eb1f7542fe2a7c0dffbb1452
c0a22043 <c0a2204357@edu.teu.ac.jp>
2023-10-02 11:35:17 +0900

    追加機能２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,12 +15,6 @@ def is_inside_screen(rect):
         0 <= rect.bottom <= HEIGHT
     )
 
-def load_images():
-    kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img_right = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_img_left = pg.transform.flip(kk_img_right, True, False)
-    return kk_img_right, kk_img_left
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -28,15 +22,22 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
 
-    kk_img_right, kk_img_left = load_images()
+    kk_img_right = pg.image.load("ex02/fig/3.png")
+    kk_img_left = pg.transform.flip(kk_img_right, True, False)
+    kk_img_right = pg.transform.rotozoom(kk_img_right, 0, 2.0)
+    kk_img_left = pg.transform.rotozoom(kk_img_left, 0, 2.0)
     kk_img = kk_img_right
 
-    bomb_surface = pg.Surface((20, 20), pg.SRCALPHA)
-    pg.draw.circle(bomb_surface, (255, 0, 0), (10, 10), 10)
+    bomb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r), pg.SRCALPHA)
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bomb_imgs.append(bb_img)
+    bomb_img = bomb_imgs[0]
 
-    bomb_surface.set_colorkey((0, 0, 0))
+    #bomb_surface.set_colorkey((0, 0, 0))
 
-    bomb_rect = bomb_surface.get_rect(topleft=(random.randint(0, WIDTH-20), random.randint(0, HEIGHT-20)))
+    bomb_rect = bomb_img.get_rect(topleft=(random.randint(0, WIDTH-bomb_img.get_width()), random.randint(0, HEIGHT - bomb_img.get_height())))
 
     clock = pg.time.Clock()
 
@@ -50,6 +51,7 @@ def main():
     }
 
     vx, vy = 5, 5
+    accs = [a for a in range(1, 11)]
 
     tmr = 0
     while True:
@@ -65,17 +67,17 @@ def main():
                 total_movement[0] += movement[0]
                 total_movement[1] += movement[1]
 
-        if total_movement[0] < 0:
-            kk_img = kk_img_right
-        elif total_movement[0] > 0:
-            kk_img = kk_img_left
 
         kk_rect.move_ip(*total_movement)
 
         if not is_inside_screen(kk_rect):
             kk_rect.move_ip(*[-movement for movement in total_movement])
 
-        bomb_rect.move_ip(vx, vy)
+        # 加速度と拡大爆弾Surfaceを選択
+        avx, avy = vx * accs[min(tmr // 500, 9)], vy * accs[min(tmr // 500, 9)]
+        bomb_img = bomb_imgs[min(tmr // 500, 9)]
+
+        bomb_rect.move_ip(avx, avy)
 
         if not is_inside_screen(bomb_rect):
             vx = -vx
@@ -84,11 +86,16 @@ def main():
         
         if kk_rect.colliderect(bomb_rect):
             return
+        
+        if total_movement[0] < 0:
+            kk_img = kk_img_right
+        elif total_movement[0] > 0:
+            kk_img = kk_img_left
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rect.topleft)
 
-        screen.blit(bomb_surface, bomb_rect)
+        screen.blit(bomb_img, bomb_rect.topleft)
 
         pg.display.update()
         tmr += 1
